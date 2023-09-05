cedc3c1301812a2b07a681d2dc97048d40734730
Luís Henrique <luishsilva09@gmail.com>
2023-09-05 20:45:33 -0300

    fix: dados para conexao

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   port: parseInt(process.env.DB_PORT ?? "3306"),
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
 });
 
